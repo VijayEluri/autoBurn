35f6cec14d6d821b9e5c8c67d9983b63aeb4bb36
malys <malys@mageos.com>
2015-05-14 18:51:07 +0200

    Add device parameter

--- burn.bat
+++ burn.bat
@@ -1,9 +1,6 @@
 Z:/D/util/CDBurnerXP/cdbxpcmd.exe --erase 
 Z:/D/util/CDBurnerXP/cdbxpcmd.exe --eject
 Z:/D/util/CDBurnerXP/cdbxpcmd.exe --load
-Z:/D/util/CDBurnerXP/cdbxpcmd.exe --burn-audio -device:0 -file[\]:"Z:\B\perso\sylvain\podcast\Podcasts\Cast-IT\02 LyonJUG - 20_11_2011 - 2nde parti.mp3" -file[\]:"Z:\B\perso\sylvain\podcast\Podcasts\FSPod _ Podcast, Blog et Science sur Fut\PodcastFSHebdo-9au15janvier.mp3" 
+Z:/D/util/CDBurnerXP/cdbxpcmd.exe --burn-audio -device:0 
 Z:/D/util/CDBurnerXP/cdbxpcmd.exe --eject
-del "Z:\B\perso\sylvain\podcast\Podcasts\Cast-IT\02 LyonJUG - 20_11_2011 - 2nde parti.mp3"
-del "Z:\B\perso\sylvain\podcast\Podcasts\FSPod _ Podcast, Blog et Science sur Fut\PodcastFSHebdo-9au15janvier.mp3"
-del "Z:\B\perso\sylvain\podcast\Podcasts\Cast-IT\02 LyonJUG - 20_11_2011 - 2nde parti.mp3"
-del "Z:\B\perso\sylvain\podcast\Podcasts\FSPod _ Podcast, Blog et Science sur Fut\PodcastFSHebdo-9au15janvier.mp3"
+pause
